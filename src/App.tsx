bc76cbdcfd8863bfaba6f84f45d9404d28c14303
AnnaDrProgrammer <annamaxi358@mail.ru>
2025-03-04 01:20:34 +0500

    added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,24 @@ import Button from './shared/ui/button';
 function App() {
   return (
     <>
-      123
-      <Button />
+      <Button view="primary" size="m">
+        Browse Trip
+      </Button>
+      <Button view="primary" size="l">
+        View Trip Details
+      </Button>
+      <Button view="secondaryDark" size="smallest">
+        Home
+      </Button>
+      <Button view="secondaryDark" size="xxs">
+        Contact us
+      </Button>
+      <Button view="secondaryLight" size="s">
+        See all
+      </Button>
+      <Button view="secondaryLight" size="xs">
+        View All Trips
+      </Button>
     </>
   );
 }
